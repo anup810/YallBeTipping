7e5ee47a3b28c20277c43348eff4a898f5c24a32
Anup Saud <anupsaud10@gmail.com>
2025-01-06 18:30:33 -0500

    Add Project Folder Structure

---
+++ YallBeTipping/Home/HomeViewController.swift
@@ -0,0 +1,19 @@
+//
+//  HomeViewController.swift
+//  YallBeTipping
+//
+//  Created by Anup Saud on 2025-01-06.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
