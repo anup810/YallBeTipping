cc47c18a28d5434dee7d1d644a8f6367d32bfce7
Anup Saud <anupsaud10@gmail.com>
2025-01-07 22:39:11 -0500

    Add reusable ConfirmCell XIB and integrate cart item display in ConfirmVC

--- YallBeTipping/Confirm/ConfirmCell.swift
+++ YallBeTipping/Confirm/ConfirmCell.swift
@@ -1,38 +1,34 @@
 //
-//  HomeTableCell.swift
+//  ConfirmCell.swift
 //  YallBeTipping
 //
-//  Created by Anup Saud on 2025-01-06.
+//  Created by Anup Saud on 2025-01-07.
 //
 
 import UIKit
-protocol HomeTableCellDelegate: AnyObject{
+protocol ConfirmCellDelegate: AnyObject{
     func didAdd(at indexPath: IndexPath)
     func didMinus(at indexPath: IndexPath)
-    
 }
 
-class HomeTableCell: UITableViewCell {
-    weak var delegate: HomeTableCellDelegate?
+class ConfirmCell: UITableViewCell {
     var indexPath: IndexPath?
-
+    var delegate:ConfirmCellDelegate?
+    @IBOutlet weak var img: UIImageView!
     @IBOutlet weak var priceLabel: UILabel!
     @IBOutlet weak var nameLabel: UILabel!
-    @IBOutlet weak var img: UIImageView!
-    @IBOutlet weak var plusButton: UIButton!
-    @IBOutlet weak var countLabel: UILabel!
     @IBOutlet weak var minusButton: UIButton!
+    @IBOutlet weak var countLabel: UILabel!
+    @IBOutlet weak var plusButton: UIButton!
     
-    @IBOutlet weak var orderCountStackView: UIStackView!
     override func awakeFromNib() {
         super.awakeFromNib()
+        // Initialization code
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
 
-
-   
     }
     override func layoutSubviews() {
         super.layoutSubviews()
@@ -40,29 +36,22 @@ class HomeTableCell: UITableViewCell {
         plusButton.layer.cornerRadius = plusButton.frame.height / 2
         minusButton.layer.cornerRadius = minusButton.frame.height / 2
     }
-    func configure(item: MenuItem, indexPath: IndexPath){
+    func configure(item:MenuItem, indexPath: IndexPath){
         self.indexPath = indexPath
         nameLabel.text = item.name
         priceLabel.text = "\(item.price)"
         img.image = UIImage(named: item.img)
         countLabel.text = "\(item.count)"
-        if item.count > 0{
-            orderCountStackView.isHidden = false
-        } else{
-            orderCountStackView.isHidden = true
-            
-        }
-        
         
     }
-
-    @IBAction func didTapPlusButton(_ sender: UIButton) {
+    
+    
+    @IBAction func didTapPlusButton() {
         guard let indexPath else {return}
         delegate?.didAdd(at: indexPath)
-    
     }
     
-    @IBAction func didTaoMinusButton(_ sender: UIButton) {
+    @IBAction func didTapMinusButton() {
         guard let indexPath else {return}
         delegate?.didMinus(at: indexPath)
     }
--- YallBeTipping/Confirm/ConfirmVC.swift
+++ YallBeTipping/Confirm/ConfirmVC.swift
@@ -10,6 +10,7 @@ import UIKit
 class ConfirmVC: UIViewController {
     let items: [MenuItem]
     
+    @IBOutlet weak var tableView: UITableView!
     init(coder:NSCoder,items:[MenuItem]){
         self.items = items
         super.init(coder: coder)!
@@ -22,10 +23,48 @@ class ConfirmVC: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupTableView()
 
         print(items)
     }
+    func setupTableView(){
+        tableView.dataSource = self
+        tableView.delegate = self
+        tableView.register(UINib(nibName: "ConfirmCell", bundle: nil), forCellReuseIdentifier: "ConfirmCell")
+        
+    }
+
+}
+extension ConfirmVC: UITableViewDataSource{
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return items.count
+    }
     
-
-
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ConfirmCell") as! ConfirmCell
+        let item = items[indexPath.row]
+        cell.configure(item: item, indexPath: indexPath)
+        cell.delegate = self
+        return cell
+        
+    }
+    
+    
+}
+extension ConfirmVC: UITableViewDelegate{
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 125
+    }
+    
+}
+extension ConfirmVC: ConfirmCellDelegate{
+    func didAdd(at indexPath: IndexPath) {
+        //
+    }
+    
+    func didMinus(at indexPath: IndexPath) {
+        //
+    }
+    
+    
 }
