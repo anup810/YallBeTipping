7c19233b87bad37a54d61aa7f63bea942df33a7e
Anup Saud <anupsaud10@gmail.com>
2025-01-08 20:50:00 -0500

    Create UI for TipVC, connect IBOutlets/IBActions, add bar button, and handle orientation changes

--- YallBeTipping/Tip/TipVC.swift
+++ YallBeTipping/Tip/TipVC.swift
@@ -8,6 +8,13 @@
 import UIKit
 
 class TipVC: UIViewController {
+    lazy var barButton: UIBarButtonItem = {
+        let button = UIBarButtonItem(title: "Submit", style: .plain, target: self, action: #selector(didTapBarButton))
+        return button
+    }()
+
+    @IBOutlet weak var stackView: UIStackView!
+    @IBOutlet weak var skipButton: UIButton!
     var items:[MenuItem]
     init(coder:NSCoder,items:[MenuItem]){
         self.items = items
@@ -22,9 +29,33 @@ class TipVC: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         print(items)
-
-        // Do any additional setup after loading the view.
+        navigationItem.rightBarButtonItem = barButton
+        skipButton.layer.cornerRadius = 15
+        stackView.arrangedSubviews.forEach { button in
+            button.layer.cornerRadius = 20
+        }
+       
+    }
+    override func viewWillTransition(to size: CGSize, with coordinator: any UIViewControllerTransitionCoordinator) {
+        let orientation = UIDevice.current.orientation
+        if orientation.isPortrait{
+            stackView.axis = .vertical
+            
+        }else if orientation.isLandscape{
+            stackView.axis = .horizontal
+        }
+    }
+    @objc func didTapBarButton(){}
+    
+    @IBAction func didTapSmallTipButton() {
     }
     
-
+    @IBAction func didTapMediumTopButton() {
+    }
+    
+    @IBAction func didTapLargeTipButton() {
+    }
+    
+    @IBAction func didTapSkipButton() {
+    }
 }
